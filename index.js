37838e4acb8d0e32c6767c4df72bf8c03a31293a
Robert Gill <RobertGill@etech.net>
2020-10-06 14:21:35 +0100

    ignore __build

--- index.js
+++ index.js
@@ -26,9 +26,12 @@ const replace = folder => {
         .then(([...stats]) => {
           const folders = stats.filter(s => s.stats.isDirectory())
           const files = stats.filter(s => s.stats.isFile())
+
           const proms = []
+
           proms.push(...folders.filter(f => !f.path.includes('dist')).map(f => replace(path.join(folder, f.path))))
           proms.push(...files.filter(f => f.path.endsWith('.yml')).map(f => replace_file(path.join(folder, f.path))))
+
           return Promise.all(proms)
         }) 
     })
@@ -37,32 +40,38 @@ const scannedValues = []
 
 const replace_file = local_path => {
   console.log('File: ', local_path)
+
   const bits = local_path.split('\\')
   bits.pop()
+
   const folder = path.join(target_path, bits.join('\\'))
 
   return fs.readFile(path.join(source, local_path), 'utf8')
     .then(data => {
-      const res = data.match(/__(\w+)__/g)
-      // console.log('Matched: ', res)
+      const res = data.match(/__(\w+)__/g).filter(x => !x.startsWith('__BUILD'))
+
       let output = data
       if (!res) {
         return console.log('no tokens in file', local_path)
       }
+
       if (mode === 'scan') {
         scannedValues.push(...res.map(x => x.split('__').join('')))
         return
       }
+
       if (mode === 'generate') {
         res.map(x => {
           const variable = x.split('__').join('')
           const replacement = values[variable]
+
           if (!replacement) {
             console.error(`Could not find value for ${variable}`)
           } else {
             output = output.split(x).join(replacement)
           }
         })
+
         const output_path = path.join(target_path, local_path)
         return fs.mkdirp(folder)
           .then(fs.writeFile(output_path, output))
